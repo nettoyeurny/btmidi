854ebf94257b12a24f27291d3a42940bf395a38b
Peter Brinkmann <peter.brinkmann@gmail.com>
2012-11-19 21:31:05 -0500

    minor update

--- UsbMidi/src/com/noisepages/nettoyeur/usbmidi/UsbMidiInterface.java
+++ UsbMidi/src/com/noisepages/nettoyeur/usbmidi/UsbMidiInterface.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Peter Brinkmann (peter.brinkmann@gmail.com)
+ * Copyright (C) 2012 Peter Brinkmann (peter.brinkmann@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
