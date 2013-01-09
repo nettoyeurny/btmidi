0f122e22a678ed05e2246b5f5dfcc10be2447995
Peter Brinkmann <peter.brinkmann@gmail.com>
2013-01-09 14:07:41 -0500

    .

--- UsbMidi/src/com/noisepages/nettoyeur/usbmidi/UsbMidiDevice.java
+++ UsbMidi/src/com/noisepages/nettoyeur/usbmidi/UsbMidiDevice.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Peter Brinkmann (peter.brinkmann@gmail.com)
+ * Copyright (C) 2013 Peter Brinkmann (peter.brinkmann@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
